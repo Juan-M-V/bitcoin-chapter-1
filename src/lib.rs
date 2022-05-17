3488284d6c5f1712547d75f31cb2959b93742ffc
Juan-M-V <juan.viel@lambdaclass.com>
2022-05-17 18:41:25 -0300

    Implement substraction

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@ use std::ops::{Add, Sub, Mul, Div};
 
 #[derive(Debug, PartialEq, Eq, PartialOrd)]
 pub struct FieldElement {
-    pub prime: u64,
-    pub num: u64
+    pub prime: i64,
+    pub num: i64
 }
 
 impl FieldElement {
-    pub fn new(value: u64, prime: u64) -> FieldElement {
+    pub fn new(value: i64, prime: i64) -> FieldElement {
         FieldElement { prime, num: (value % prime) }
     }
 }
@@ -27,7 +27,18 @@ impl Add for FieldElement {
     }
 }
 
+impl Sub for FieldElement {
+    type Output = FieldElement;
 
+    fn sub(self, y: FieldElement) -> Self::Output {
+        if self.prime != y.prime {
+            panic!("Both elements must belong to fields of the same size.")
+        }
+
+        let num = (self.num - y.num).rem_euclid(self.prime);
+        FieldElement {prime: self.prime, num }
+    }
+}
 
 #[test]
 fn field_elements_can_be_created() {
@@ -43,3 +54,12 @@ fn field_elements_can_be_added() {
 
     assert_eq!(FieldElement::new(0, 3), x + y)
 }
+
+#[test]
+fn field_elements_can_be_substracted() {
+    let x = FieldElement::new(2,5);
+    let y = FieldElement::new(3,5);
+
+    assert_eq!(FieldElement::new(4,5), x - y)
+}
+
