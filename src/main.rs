e21cd43fe26936048f49e19f33a93475d7afcfe7
juanbono <juan.bono@nextroll.com>
2022-05-17 19:16:55 -0300

    cargo clippy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bitcoin_chapter1::FieldElement;
 
 fn main() {
-    let fieldElem = FieldElement {prime: 2, num: 1};
-    println!("{:?}", fieldElem);
+    let field_elem = FieldElement {prime: 2, num: 1};
+    println!("{:?}", field_elem);
 }
